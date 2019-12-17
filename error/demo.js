1df092bbe8eda2dca41c221c70bd71761d7a0ef9
ZHANGHD0715 <haidonghi@126.com>
2019-12-17 13:46:56 +0800

    Add

--- error/demo.js
+++ error/demo.js
@@ -2,6 +2,7 @@ function thr() {
   try {
     // connnnsole.log("this is a error");
     throw "first error"; 
+    console.log("not going to execute...")
   } catch(err) {
     console.log("catched...", err)
     throw "second error"; 
